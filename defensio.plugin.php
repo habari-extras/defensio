4d69d6f435242f54fb5914ce797c68a3d53018ba
michaeltwofish <USER@DOMAIN>
2008-08-19 23:37:17 +0000

    Show an error on the dashboard, rather than dying, if Defensio returns a bad response. Fixes #57. Thanks ayunyan.
    git-svn-id: http://svn.habariproject.org/habari-extras/plugins/defensio/trunk@774 f4a90942-1786-4aa1-b092-57c31935ddc3

--- defensio.plugin.php
+++ defensio.plugin.php
@@ -44,7 +44,7 @@ class Defensio extends Plugin
 
 	public function filter_dash_modules( $modules )
 	{
-		$modules[]= 'Defensio';
+		$modules[] = 'Defensio';
 		$this->add_template( 'dash_defensio', dirname( __FILE__ ) . '/dash_defensio.php' );
 		return $modules;
 	}
@@ -52,7 +52,7 @@ class Defensio extends Plugin
 	public function filter_plugin_config( $actions, $plugin_id )
 	{
 		if ( $plugin_id == $this->plugin_id() ) {
-			$actions[]= _t('Configure', 'defensio');
+			$actions[] = _t('Configure', 'defensio');
 		}
 		return $actions;
 	}
@@ -65,15 +65,15 @@ class Defensio extends Plugin
 					$ui = new FormUI( 'defensio' );
 
 					// Add a text control for the address you want the email sent to
-					$api_key= $ui->append( 'text', 'api_key', 'option:defensio__api_key', _t('Defensio API Key: ', 'defensio') );
+					$api_key = $ui->append( 'text', 'api_key', 'option:defensio__api_key', _t('Defensio API Key: ', 'defensio') );
 					$api_key->add_validator( 'validate_required' );
 					$api_key->add_validator( array( $this, 'validate_api_key' ) );
 
-					$announce_posts= $ui->append( 'select', 'announce_posts', 'option:defensio__announce_posts', _t('Announce New Posts To Defensio: ', 'defensio') );
-					$announce_posts->options= array( 'yes' => _t('Yes', 'defensio'), 'no' => _t('No', 'defensio') );
+					$announce_posts = $ui->append( 'select', 'announce_posts', 'option:defensio__announce_posts', _t('Announce New Posts To Defensio: ', 'defensio') );
+					$announce_posts->options = array( 'yes' => _t('Yes', 'defensio'), 'no' => _t('No', 'defensio') );
 					$announce_posts->add_validator( 'validate_required' );
 
-					$register= $ui->append( 'static', 'register', '<a href="http://defensio.com/signup">' . _t('Get A New Defensio API Key.', 'defensio') . '</a>' );
+					$register = $ui->append( 'static', 'register', '<a href="http://defensio.com/signup">' . _t('Get A New Defensio API Key.', 'defensio') . '</a>' );
 
 					$ui->append( 'submit', 'save', _t( 'Save', 'defensio' ) );
 					$ui->set_option( 'success_message', _t( 'Configuration saved', 'defensio' ) );
@@ -96,7 +96,7 @@ class Defensio extends Plugin
 
 	public function action_init()
 	{
-		$this->defensio= new DefensioAPI( Options::get( 'defensio__api_key' ), Site::get_url( 'habari' ) );
+		$this->defensio = new DefensioAPI( Options::get( 'defensio__api_key' ), Site::get_url( 'habari' ) );
 		$this->load_text_domain( 'defensio' );
 		$this->add_template( 'defensio', dirname(__FILE__) . '/moderate.php' );
 	}
@@ -115,20 +115,26 @@ class Defensio extends Plugin
 
 	function filter_adminhandler_post_loadplugins_main_menu( $menu )
 	{
-		$menu['spam']= array( 'url' => URL::get( 'admin', 'page=spam' ), 'title' => _t('Manage all spam in the spam vault','defensio'), 'text' => _t('Spam Vault','defensio'), 'hotkey' => 'S', 'selected' => false );
+		$menu['spam'] = array( 'url' => URL::get( 'admin', 'page=spam' ), 'title' => _t('Manage all spam in the spam vault','defensio'), 'text' => _t('Spam Vault','defensio'), 'hotkey' => 'S', 'selected' => false );
 		return $menu;
 	}
 
 	public function filter_dash_module_defensio( $module, $module_id, $theme )
 	{
-		$stats= $this->theme_defensio_stats();
+		$stats = $this->theme_defensio_stats();
+		// Show an error in the dashboard if Defensio returns a bad response.
+		if ( !$stats ) {
+			$module['title'] = '<a href="' . Site::get_url('admin') . '/spam">' . _t('Defensio') . '</a>';
+			$module['content'] = '<ul class=items"><li class="item clear">' . _t('Bad Response From Server') . '</li></ul>';
+			return $module;
+		}
 
-		$theme->accuracy= sprintf( '%.2f', $stats->accuracy * 100 );
-		$theme->spam= $stats->spam;
-		$theme->ham= $stats->ham;
-		$theme->false_negatives= $stats->false_negatives;
-		$theme->false_positives= $stats->false_positives;
-		
+		$theme->accuracy = sprintf( '%.2f', $stats->accuracy * 100 );
+		$theme->spam = $stats->spam;
+		$theme->ham = $stats->ham;
+		$theme->false_negatives = $stats->false_negatives;
+		$theme->false_positives = $stats->false_positives;
+
 		$module['title'] = '<a href="' . Site::get_url('admin') . '/spam">' . _t('Defensio') . '</a>';
 		$module['content'] = $theme->fetch( 'dash_defensio' );
 		return $module;
@@ -137,11 +143,11 @@ class Defensio extends Plugin
 	public function theme_defensio_stats()
 	{
 		if ( Cache::has( 'defensio_stats' ) ) {
-			$stats= Cache::get( 'defensio_stats' );
+			$stats = Cache::get( 'defensio_stats' );
 		}
 		else {
 			try {
-				$stats= $this->defensio->get_stats();
+				$stats = $this->defensio->get_stats();
 				Cache::set( 'defensio_stats', $stats );
 			}
 			catch ( Exception $e ) {
@@ -155,8 +161,8 @@ class Defensio extends Plugin
 
 	public function action_comment_insert_before( $comment )
 	{
-		$user= User::identify();
-		$params= array(
+		$user = User::identify();
+		$params = array(
 			'user-ip' => long2ip( $comment->ip ),
 			'article-date' => date( 'Y/m/d', strtotime( $comment->post->pubdate ) ),
 			'comment-author' => $comment->name,
@@ -168,22 +174,22 @@ class Defensio extends Plugin
 			'referrer' => isset( $_SERVER['HTTP_REFERER'] ) ? $_SERVER['HTTP_REFERER'] : null,
 		);
 		if ( $user instanceof User ) {
-			$params['user-logged-in']= $user instanceof User;
+			$params['user-logged-in'] = $user instanceof User;
 			// test for administrator, editor, etc. as well
-			$params['trusted-user']= $user instanceof User;
+			$params['trusted-user'] = $user instanceof User;
 			if ( $user->info->openid_url ) {
-				$params['openid']= $user->info->openid_url;
+				$params['openid'] = $user->info->openid_url;
 			}
 		}
 
 		try {
-			$result= $this->defensio->audit_comment( $params );
+			$result = $this->defensio->audit_comment( $params );
 			if ( $result->spam == true ) {
-				$comment->status= 'spam';
-				$comment->info->spamcheck= array_unique(array_merge((array) $comment->info->spamcheck, array( _t('Flagged as Spam by Defensio', 'defensio'))));
+				$comment->status = 'spam';
+				$comment->info->spamcheck = array_unique(array_merge((array) $comment->info->spamcheck, array( _t('Flagged as Spam by Defensio', 'defensio'))));
 			}
-			$comment->info->defensio_signature= $result->signature;
-			$comment->info->defensio_spaminess= $result->spaminess;
+			$comment->info->defensio_signature = $result->signature;
+			$comment->info->defensio_spaminess = $result->spaminess;
 		}
 		catch ( Exception $e ) {
 			EventLog::log( $e->getMessage(), 'notice', 'comment', 'Defensio' );
@@ -192,21 +198,21 @@ class Defensio extends Plugin
 
 	public function action_admin_moderate_comments( $action, $comments, $handler )
 	{
-		$false_positives= array();
-		$false_negatives= array();
+		$false_positives = array();
+		$false_negatives = array();
 
 		foreach ( $comments as $comment ) {
 			switch ( $action ) {
 				case 'spam':
 					if ( ( $comment->status == Comment::STATUS_APPROVED || $comment->status == Comment::STATUS_UNAPPROVED )
 						&& isset($comment->info->defensio_signature) ) {
-						$false_negatives[]= $comment->info->defensio_signature;
+						$false_negatives[] = $comment->info->defensio_signature;
 						Cache::expire('defensio_stats');
 					}
 					break;
 				case 'approve':
 					if ( $comment->status == Comment::STATUS_SPAM && isset($comment->info->defensio_signature) ) {
-						$false_positives[]= $comment->info->defensio_signature;
+						$false_positives[] = $comment->info->defensio_signature;
 						Cache::expire('defensio_stats');
 					}
 					break;
@@ -231,7 +237,7 @@ class Defensio extends Plugin
 	public function action_post_insert_after( $post )
 	{
 		if ( Options::get( 'defensio__announce_posts' ) == 'yes' && $post->statusname == 'published' ) {
-			$params= array(
+			$params = array(
 				'article-author' => $post->author->username,
 				'article-author-email' => $post->author->email,
 				'article-title' => $post->title,
@@ -240,7 +246,7 @@ class Defensio extends Plugin
 			);
 
 			try {
-				$result= $this->defensio->announce_article( $params );
+				$result = $this->defensio->announce_article( $params );
 			}
 			catch ( Exception $e ) {
 				EventLog::log( $e->getMessage(), 'notice', 'content', 'Defensio' );
