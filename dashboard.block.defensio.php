b9a07aa77cc9c1e998ac1e64ab44d5ed64e75950
Matt Read <matt@mattread.com>
2012-11-29 19:01:10 -0500

    I don't know what I'm doing

--- dashboard.block.defensio.php
+++ dashboard.block.defensio.php
@@ -1,25 +1,25 @@
-<?php if ( $error ) : ?>
+<?php if ( $content->error ) : ?>
 	<ul class=items">
 		<li class="item clear">
-			<?php echo $error; ?>
+			<?php echo $content->error; ?>
 		</li>
 	</ul>
 <?php else : ?>
 	<ul class=items">
 		<li class="item clear">
-			<span class="title pct80"><b>Recent Accuracy</b></span><span class="comments pct20"><?php echo $accuracy; ?>%</span>
+			<span class="title pct80"><b>Recent Accuracy</b></span><span class="comments pct20"><?php echo $content->accuracy; ?>%</span>
 		</li>
 		<li class="item clear">
-			<span class="pct80">Spam</span><span class="comments pct20"><?php echo $spam; ?></span>
+			<span class="pct80">Spam</span><span class="comments pct20"><?php echo $content->spam; ?></span>
 		</li>
 		<li class="item clear">
-			<span class="pct80">Innocents</span><span class="comments pct20"><?php echo $ham; ?></span>
+			<span class="pct80">Innocents</span><span class="comments pct20"><?php echo $content->ham; ?></span>
 		</li>
 		<li class="item clear">
-			<span class="pct80">False Negatives</span><span class="comments pct20"><?php echo $false_negatives; ?></span>
+			<span class="pct80">False Negatives</span><span class="comments pct20"><?php echo $content->false_negatives; ?></span>
 		</li>
 		<li class="item clear">
-			<span class="pct80">False Positives</span><span class="comments pct20"><?php echo $false_positives; ?></span>
+			<span class="pct80">False Positives</span><span class="comments pct20"><?php echo $content->false_positives; ?></span>
 		</li>
 	</ul>
 <?php endif; ?>
